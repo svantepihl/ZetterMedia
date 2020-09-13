390812fdc072a04c021d3ec74ac9dc93990d0f69
Svante Pihl <hej@svantepihl.com>
2020-09-14 01:43:14 +0200

    Added tailwind and finished background video and text animation

---
+++ src/components/Services.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="services" class=" relative bg-white w-screen h-screen">
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Services",
+}
+</script>
+
+<style scoped>
+#services{
+  padding-top: 100vh;
+}
+
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,50 +1,24 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot/>
-  </div>
+	<div class="min-w-screen min-h-screen m-0 p-0">
+		<Navbar></Navbar>
+		<Video></Video>
+		<Services></Services>
+	</div>
 </template>
 
-<static-query>
-query {
-  metadata {
-    siteName
-  }
-}
-</static-query>
+<script>
+	import Navbar from '../components/Navbar.vue';
+	import Video from '../components/Video.vue';
+	import Services from '../components/Services.vue'
+  	export default {
+		  components: {Navbar,Video,Services}
+	}
+</script>
 
 <style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
+body{
+	background-color: gray;
 }
 
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
 </style>
