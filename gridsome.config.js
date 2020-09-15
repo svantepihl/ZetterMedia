105cfb6f191a3d64ada8834f2d008722f5df70ea
Svante Pihl <hej@svantepihl.com>
2020-09-15 11:06:22 +0200

    remvoved github workflows

--- gridsome.config.js
+++ gridsome.config.js
@@ -15,3 +15,43 @@ module.exports = {
 	},  
   ]
 };
+
+module.exports = {
+  siteName: 'ZetterMedia',
+  siteDescription: "INFO OM ZETTERMEDIA",
+  siteUrl: 'https://zettermedia.se',
+  titleTemplate: `%s | ZetterMedia AB`,
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        {
+          use: "gridsome-plugin-tailwindcss"
+        }
+      ]
+    }
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'posts/**/*.md',
+        typeName: 'Post',
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    },
+  ]
+}
